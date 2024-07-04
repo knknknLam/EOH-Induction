97707b3adaa887a5c9c9e0f39b72748a048176f3
knknknLam <kn-mandy.lam@haesl.com>
2024-07-04 09:32:54 +0800

    eoh-induction

---
+++ story_content/triggers.js
@@ -0,0 +1,180 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6JujYNGPZ0c":
+        Script1();
+        break;
+      case "5hGHwHANB91":
+        Script2();
+        break;
+      case "68bfFd7GwvJ":
+        Script3();
+        break;
+      case "5g9GfS0HJOT":
+        Script4();
+        break;
+      case "5nDD1sQWsAb":
+        Script5();
+        break;
+      case "5vY0PhGSyDe":
+        Script6();
+        break;
+      case "68Mi3zit7tn":
+        Script7();
+        break;
+      case "6DYJXs3D3Ur":
+        Script8();
+        break;
+      case "5wocodcGjZC":
+        Script9();
+        break;
+      case "6BFZTZmmd4j":
+        Script10();
+        break;
+      case "5e4XVvDjRVz":
+        Script11();
+        break;
+      case "5Uh2lFgBpF7":
+        Script12();
+        break;
+      case "63Q1biz4JEY":
+        Script13();
+        break;
+      case "6c0m0LWuTSz":
+        Script14();
+        break;
+      case "6SoHJ0VlHz6":
+        Script15();
+        break;
+      case "5tn7ppiGOzq":
+        Script16();
+        break;
+      case "6qoCrbulR0c":
+        Script17();
+        break;
+      case "5qtRRirPHdr":
+        Script18();
+        break;
+      case "6CtXAxf40PP":
+        Script19();
+        break;
+      case "5rmcPWKmq6d":
+        Script20();
+        break;
+      case "6jQ9rILZ6if":
+        Script21();
+        break;
+      case "5ygT2geiouZ":
+        Script22();
+        break;
+      case "6EfTsffB3Pw":
+        Script23();
+        break;
+      case "5kAVAXNUJ1m":
+        Script24();
+        break;
+      case "5zh3sCXOoa5":
+        Script25();
+        break;
+      case "5cxKJ9mxwTS":
+        Script26();
+        break;
+      case "69K0T94yuio":
+        Script27();
+        break;
+      case "6262VoSivDJ":
+        Script28();
+        break;
+      case "6OoRKLPR6FK":
+        Script29();
+        break;
+      case "6FVhhSJIYpL":
+        Script30();
+        break;
+      case "6dsE24mqPlb":
+        Script31();
+        break;
+      case "5ngB0r1E9kV":
+        Script32();
+        break;
+      case "6NnEn9eDIQ3":
+        Script33();
+        break;
+      case "5brAmrBnLCF":
+        Script34();
+        break;
+      case "6qOTEMVXaf2":
+        Script35();
+        break;
+      case "6jKpLZH37kR":
+        Script36();
+        break;
+      case "5mgWc4qZoDn":
+        Script37();
+        break;
+      case "6FtwYaNT4fc":
+        Script38();
+        break;
+      case "67tSrcXAfhS":
+        Script39();
+        break;
+      case "67BjiiTFAHS":
+        Script40();
+        break;
+      case "6g6z5qOas24":
+        Script41();
+        break;
+      case "6N8wEb5kbu0":
+        Script42();
+        break;
+      case "6jRNZEzJ1HK":
+        Script43();
+        break;
+      case "6bln0eaKsID":
+        Script44();
+        break;
+      case "5V0sMnQQJ5A":
+        Script45();
+        break;
+      case "5k1q8m94kC7":
+        Script46();
+        break;
+      case "5sncy6KJC9g":
+        Script47();
+        break;
+      case "5mAf2yX9Jzz":
+        Script48();
+        break;
+      case "6FULBEo8RSq":
+        Script49();
+        break;
+      case "5fcKi9mqhZi":
+        Script50();
+        break;
+      case "5jbzB6x7gPq":
+        Script51();
+        break;
+      case "67dpOyiASJa":
+        Script52();
+        break;
+      case "6lklxLCOgeX":
+        Script53();
+        break;
+      case "6Y8QksZ3EWW":
+        Script54();
+        break;
+      case "6LilZHT8PgX":
+        Script55();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
